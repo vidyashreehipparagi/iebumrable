6092c0091f1243270868820f4b907b098c59968b
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-07-11 11:48:06 +0530

    Add project files.

---
+++ iebumrable/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iebumrable
+{
+    public class Player
+    {
+        private int runs;
+        private string name;
+
+        public Player(int runs, string name)
+        {
+            this.runs = runs;
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return $"{name}-->{runs}";
+        }
+    }
+    public class Team:IEnumerable
+    {
+        Player[] players;
+        public Team()
+        {
+            players = new Player[3];
+            players[0] = new Player(12000, "Sachin");
+            players[1] = new Player(11000, "Dhoni");
+            players[2] = new Player(10000, "Virat");
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return players.GetEnumerator();
+        }
+    }
+}
---
+++ iebumrable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iebumrable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Team india= new Team();
+            foreach(Player p in india)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
